597f2608756a6426775b221c442bf508292569fc
root <root@bryivm.moscow.ctm.ru>
2021-04-22 17:33:10 +0300

    more fcn

--- flask_frontend.py
+++ flask_frontend.py
@@ -4,13 +4,19 @@ import os
 
 app = Flask(__name__)
 
+requests_array = []
+di = {}
 @app.route("/", methods=['post', 'get'])
 def index():
     if request.method == 'POST':
 	    x = str(request.form.get('x'))  # запрос к данным формы
     try:
+        requests_array.append(x)
+        requests_array_str = ' '.join(requests_array)
         a = requests.get('http://api:5000/?x={}'.format(x))
         result = int(a.content)
+        di[x] = result
+        return render_template('index.html', variable=result, variable2=requests_array_str, variable3=di)
     except:
         result = 0
-    return render_template('index.html', variable=result)
+        return render_template('index.html', variable=result, variable3=di)
