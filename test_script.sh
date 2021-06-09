7177af0539e18df10e863650445330f2a6d02607
root <a.sazonov@ctm.ru>
2021-06-09 13:23:27 +0000

    aws ok works

--- test_script.sh
+++ test_script.sh
@@ -4,13 +4,13 @@ END=40
 do
 for i in $(shuf -i 1-$END -n 1)
 do 
-result=`curl -s 192.168.49.2/api/fib/?x=$i`;
+result=`curl -s http://api.bryidomain.tk/fib/?x=$i`;
 #dict=`curl -s localhost:5003/dict`;
 echo $result
 if [ "$result" == "None" ]; then
 break
 fi;
 #echo $dict
-#sleep 1
+#sleep 3
 done
 done
